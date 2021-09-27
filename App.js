f10788884fd26c4931c8110e8215cfae27f81b76
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-27 16:03:01 -0700

    Added title to the main section along with some styling, Created components folder, Created Task component with simple structure

--- App.js
+++ App.js
@@ -3,12 +3,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			{/* Today's Tasks */}
+			<View style={styles.tasksWrapper}>
+				<Text style={styles.sectionTitle}>Today's tasks</Text>
+				<View style={styles.items}>
+					{/* This is where the tasks will go! */}
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#E8EAED',
 	},
+	tasksWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	items: {},
 });
